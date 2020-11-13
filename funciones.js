463cc769289b0f0269d158c68583c25fd7a9eb34
alexrichard1994 <alexandrorichard@hotmail.com>
2020-11-13 18:21:52 -0500

    commit final

---
+++ funciones.js
@@ -0,0 +1,57 @@
+const dos = 2
+
+const cuatro = 4
+
+const add = (dos+cuatro)
+
+alert("la addicion da:"+add)
+
+//
+
+const veinte = 20
+
+const treinta = 30
+
+const menos = (treinta-veinte)
+
+alert("la substraccion da:"+menos)
+
+//
+
+let frase = "La Voz De Tu Cabeza Siempre Tiene El Mismo Volumen"
+
+console.log (frase)
+
+console.log (frase.toUpperCase())
+
+console.log (frase.toLowerCase())
+
+//
+
+const espacios = "   aqui  hay espacios   al azar .  "
+
+console.log (espacios)
+
+console.log (espacios.trim())
+
+//
+
+const cinco = "un texto de cinco palabras"
+
+//
+
+const frutas = "Banana Fresa Naranja Guayaba Limon"
+
+const frutassplit = frutas.split (" ")
+
+console.log (frutas);
+
+console.log (frutassplit);
+
+//
+
+const email = "alexandrorichard@hotmail.com"
+
+console.log (email);
+
+console.log (email.split("@"));
